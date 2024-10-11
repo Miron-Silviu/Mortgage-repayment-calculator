0ebf744e1f21f0e1274e0134ced7ff225e192e2f
Silviu <silviu_miron_90@outlook.com>
2024-10-11 15:26:17 +0100

    WIP : Selecting elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Storing classes inside variables
+
+const clearButton = document.querySelector('.header__button');
+const amount = document.querySelector('.amount__input');
+const team = document.querySelector('.team__input');
+const rate = document.querySelector('.input__interest__rate');
+const repayment = document.querySelector('.main__repayment__input');
+const interest = document.querySelector('.main__interest__input');
+const mainButton = document.querySelector('.main__button');
