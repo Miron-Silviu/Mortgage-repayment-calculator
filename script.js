81d8713c3c053a592c98a417ed507e3945fef0d6
Silviu <silviu_miron_90@outlook.com>
2024-10-21 18:17:34 +0100

    Build : Store values for each input in mainButton
    -Creat new variables to store inputs values inside mainButton.

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 'use strict';
 
 // Selecting elements
+const amount = document.querySelector('.amount__input');
+const team = document.querySelector('.team__input');
+const rate = document.querySelector('.input__interest__rate');
 
-let clearButton = document.querySelector('.header__button');
-let amount = document.querySelector('.amount__input').value;
-let team = document.querySelector('.team__input').value;
-let rate = document.querySelector('.input__interest__rate').value;
-let repayment = document.querySelector('.main__repayment__input');
-let interest = document.querySelector('.main__interest__input');
-let mainButton = document.querySelector('.main__button');
+const clearButton = document.querySelector('.header__button');
+const repayment = document.querySelector('.main__repayment__input');
+const interest = document.querySelector('.main__interest__input');
+const mainButton = document.querySelector('.main__button');
 
-//  Create funcionalty for amount input
-let amountInput;
-
-function mortgageAmount(a) {
-  for (let i = 0; i > amount.length; i++) {
-    amountInput.push([i]);
-  }
-  return amountInput;
-}
 //  Creating main button event
 
 mainButton.addEventListener('click', (e) => {
-  console.log(amount);
+  const amountInput = amount.value;
+  const teamInput = team.value;
+  const rateInput = rate.value;
+  console.log(amountInput, teamInput, rateInput);
 });
